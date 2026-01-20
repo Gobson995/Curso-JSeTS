a0b293a94dcf3e47df12b8c23afeb1da1351ca2b
Gobson995 <gugabadazanatta@gmail.com>
2026-01-19 22:35:21 -0300

    começando as aulas

---
+++ index.js
@@ -0,0 +1,12 @@
+const nome = 'Gustavo';
+var idade = 19;
+
+console.log(`Meu nome é ${nome}`); // melhor forma!
+console.log(nome, "tem", idade, "anos!");
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+let nomeAluno; // undefined -> não aponta para lugar nenhum na memória
+let sobrenomeAluno = null; // nulo -> não aponta para lugar nenhum na memória
+
