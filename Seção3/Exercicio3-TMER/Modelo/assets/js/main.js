1a761106a2184db77c2b31f8566686478f57838d
Gobson995 <gugabadazanatta@gmail.com>
2026-01-30 23:55:18 -0300

    fazendo exercicio timer

---
+++ Seção3/Exercicio3-TMER/Modelo/assets/js/main.js
@@ -0,0 +1,38 @@
+function criaHoraDosSegundos (segundos) {
+  const data = new Date (segundos * 1000);
+  return data.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'UTC'
+  });
+}
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+
+let segundos = 0;
+let timer;
+
+function iniciaTimer () {
+  clearInterval(timer); 
+
+  timer = setInterval(function() {
+    segundos++;
+    relogio.innerHTML = criaHoraDosSegundos(segundos);
+  }, 1000);
+}
+
+iniciar.addEventListener('click', function(event) {
+  iniciaTimer();
+});
+
+pausar.addEventListener('click', function(event) {
+  clearInterval(timer);
+});
+
+zerar.addEventListener('click', function(event) {
+  clearInterval(timer);
+  segundos = 0;
+  relogio.innerHTML = criaHoraDosSegundos(segundos);
+});
