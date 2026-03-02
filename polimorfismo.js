c522cbb9cb57aff88fc35c695533bc21dfa6d507
Gobson995 <gugabadazanatta@gmail.com>
2026-03-02 20:22:32 -0300

    terminando seção 6

---
+++ polimorfismo.js
@@ -0,0 +1,58 @@
+// superclass
+function Conta(agencia, conta, saldo) {
+    this.agencia = agencia;
+    this.conta = conta;
+    this.saldo = saldo;
+}
+
+Conta.prototype.sacar = function(valor) {
+    if (this.saldo < valor) {
+        console.log("Você não consegue sacar mais do que tem nada conta. Confira seus dados:");
+        this.verSaldo();
+
+        return;
+    }
+
+    this.saldo -= valor;
+    this.verSaldo();
+};
+
+Conta.prototype.depositar = function(valor) {
+    this.saldo += valor;
+    this.verSaldo();
+};
+
+Conta.prototype.verSaldo = function() {
+    console.log(
+        `AG/C: ${this.agencia}/${this.conta} | ` +
+        `Saldo: R$${this.saldo.toFixed(2)}`
+    );
+};
+
+function ContaCorrente(agencia, conta, saldo, limite) {
+    Conta.call(this, agencia, conta, saldo);
+    this.limite = limite;
+}
+
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.prototype.constructor = ContaCorrente;
+
+ContaCorrente.prototype.sacar = function(valor) {
+    if ((this.saldo + this.limite) < valor) {
+        console.log("Você não consegue sacar mais do que o limite da conta. Confira seus dados:");
+        this.verSaldo();
+
+        return;
+    }
+
+    this.saldo -= valor;
+    this.verSaldo();
+};
+
+function ContaPoupanca(agencia, conta, saldo) {
+    Conta.call(this, agencia, conta, saldo);
+}
+
+ContaPoupanca.prototype = Object.create(Conta.prototype);
+ContaPoupanca.prototype.constructor = ContaPoupanca;
+
